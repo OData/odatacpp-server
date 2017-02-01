f07e32e7c099c326f8733e52a194561f4ff4c83a
Dimitri Kaparis <kaparis.dimitri@gmail.com>
2017-02-01 13:26:19 +0200

    Add braces around else clause to avoid g++ 6.3.1 warning

--- include/odata/common/compat/SafeInt3.hpp
+++ include/odata/common/compat/SafeInt3.hpp
@@ -3247,6 +3247,7 @@ public:
                 result = (T)((__int64)t/(__int64)u);
         }
         else // Corner case
+        {
             if( t == IntTraits< T >::minInt && u == (unsigned __int64)IntTraits< T >::minInt )
             {
                 // Min int divided by it's own magnitude is -1
@@ -3257,6 +3258,7 @@ public:
                 result = 0;
             }
             return SafeIntNoError;
+        }
     }
 
     template < typename E >
